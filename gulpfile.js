ccf52d6594a7f040adea54a505169200b9e984d1
Wesley Soares <wesley.emw@gmail.com>
2018-02-24 12:11:24 -0300

    gulpfile added

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch');
+
+gulp.task('default', function() {
+  console.log("Olar");
+});
+
+gulp.task('html', function() {
+
+});
+
+gulp.task('styles', function() {
+
+});
+
+gulp.task('watch', function() {
+  
+});
